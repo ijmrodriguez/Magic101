466b5af76ce2775571c853d4423bb4ba5ea84711
ijmrodriguez <ijonmrodriguez@gmail.com>
2023-09-05 12:56:41 -0700

    Added outputs

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,36 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 public class Main
 {
     public static void main(String[] args)
     {
-        Reference karen = new Reference("Karen");
-        Dragon ice = new Dragon("Ice", "Dragon", 18, 20.000, 2, "Blue");
-        Unicorn sparkles = new Unicorn("Sparkles", "Unicorn", 200, 700.20, 7, "wool");
-        Griffith andrew = new Griffith("Andrew", "Griffith", 70, 500.16, "red", 2);
+        Reference reference = new Reference("");
+        Dragon dragon = new Dragon("", "Dragon", 18, 20.000, 2, "Blue");
+        Unicorn unicorn = new Unicorn("", "Unicorn", 200, 700.20, 7, "wool");
+        Griffith griffith = new Griffith("", "Griffith", 70, 500.16, "red", 2);
 
         ArrayList<MagicAnimal> animals = new ArrayList<>();
-        animals.add(ice);
-        animals.add(sparkles);
-        animals.add(andrew);
+        animals.add(dragon);
+        animals.add(unicorn);
+        animals.add(griffith);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Your name:");
+        reference.setName(sc.nextLine());
+
+        System.out.println("Dragon name:");
+        dragon.setName(sc.nextLine());
+
+        System.out.println("Unicorn name:");
+        unicorn.setName(sc.nextLine());
+
+        System.out.println("Griffith name:");
+        griffith.setName(sc.nextLine());
+
+        System.out.println(reference);
+        System.out.println(dragon);
+        System.out.println(unicorn);
+        System.out.println(griffith);
     }
 }
--- src/Reference.java
+++ src/Reference.java
@@ -2,7 +2,6 @@ public class Reference
 {
     //Variables
     String name;
-
     //Constructors
     public Reference(String name)
     {
